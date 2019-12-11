5d51d98acfb2856b19ed29c9dcd84985ee0a7bcd
zhangdekai <664939067@qq.com>
2019-12-11 10:25:48 +0800

    测试dispatch_applyAPI

--- iOS多线程/ViewController.m
+++ iOS多线程/ViewController.m
@@ -18,8 +18,60 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self test_dispatch_apply];
+    
 }
+
+- (void)test_dispatch_apply {
+    
+    NSLog(@"----1");
+    NSArray *array = @[@"1",@"2",@"3",@"4"];
+    
+    dispatch_apply(array.count, dispatch_get_global_queue(0, 0), ^(size_t index) {
+           
+           NSLog(@"queue 并行 ——Thread：%@",[NSThread currentThread]);
+
+           NSLog(@"元素：%@----第%ld次", array[index],index);
+           
+       });
+    
+    NSLog(@"----2");
+
+    
+    dispatch_queue_t queue = dispatch_queue_create("com.iosmuti.serial", DISPATCH_QUEUE_SERIAL);
+    
+    dispatch_apply(array.count, queue, ^(size_t index) {
+        
+        NSLog(@"queue 串行——Thread：%@",[NSThread currentThread]);
+
+        
+        NSLog(@"元素：%@----第%ld次", array[index],index);
+
+    });
+    
+    NSLog(@"----3");
+    
+    dispatch_queue_t main = dispatch_get_main_queue();
+    
+    dispatch_apply(array.count, main, ^(size_t index) {//同步 阻塞主线程
+        
+        NSLog(@"queue 串行——Thread：%@",[NSThread currentThread]);
+
+        
+        NSLog(@"元素：%@----第%ld次", array[index],index);
+
+    });
+    
+    NSLog(@"----4");
+
+    
+
+}
+
+
+
+
 
 - (IBAction)jumpToSync:(id)sender {
     
--- iOS多线程/dispatch_api/DispatchBarrierViewController.m
+++ iOS多线程/dispatch_api/DispatchBarrierViewController.m
@@ -68,13 +68,69 @@
 
 - (void)testDispatchApply {
     
+    //dispatch_apply:是同步的,可以在主线程走任务,如果想异步可以在外面包一层
+    
+    NSLog(@"----1");
     NSArray *array = @[@"1",@"2",@"3",@"4"];
     
     dispatch_apply(array.count, dispatch_get_global_queue(0, 0), ^(size_t index) {
+           
+           NSLog(@"queue 并行 ——Thread：%@",[NSThread currentThread]);
+
+           NSLog(@"元素：%@----第%ld次", array[index],index);
+           
+       });
+    
+    NSLog(@"----2");
+
+    
+    dispatch_queue_t queue = dispatch_queue_create("com.iosmuti.serial", DISPATCH_QUEUE_SERIAL);
+    
+    dispatch_apply(array.count, queue, ^(size_t index) {
+        
+        NSLog(@"queue 串行——Thread：%@",[NSThread currentThread]);
+
         
         NSLog(@"元素：%@----第%ld次", array[index],index);
-        
+
     });
+    
+    NSLog(@"----3");
+    
+    dispatch_queue_t main = dispatch_get_main_queue();
+    
+    dispatch_apply(array.count, main, ^(size_t index) {//ps:同步 阻塞主线程
+        
+        NSLog(@"queue 串行——Thread：%@",[NSThread currentThread]);
+
+        
+        NSLog(@"元素：%@----第%ld次", array[index],index);
+
+    });
+    
+    NSLog(@"----4");
+
+    
+    /*
+     queue 并行 ——Thread：<NSThread: 0x600000eb72c0>{number = 6, name = (null)}
+     2019-12-10 12:11:34.638541+0800 iOS多线程[4170:118583] queue 并行 ——Thread：<NSThread: 0x600000eb8940>{number = 5, name = (null)}
+     2019-12-10 12:11:34.638538+0800 iOS多线程[4170:118579] queue 并行 ——Thread：<NSThread: 0x600000eb2300>{number = 3, name = (null)}
+     2019-12-10 12:11:34.638565+0800 iOS多线程[4170:118478] queue 并行 ——Thread：<NSThread: 0x600000ef1cc0>{number = 1, name = main}
+     2019-12-10 12:11:34.638668+0800 iOS多线程[4170:118579] 元素：4----第3次
+     2019-12-10 12:11:34.638668+0800 iOS多线程[4170:118578] 元素：2----第1次
+     2019-12-10 12:11:34.638669+0800 iOS多线程[4170:118583] 元素：3----第2次
+     2019-12-10 12:11:34.638758+0800 iOS多线程[4170:118478] 元素：1----第0次
+     2019-12-10 12:11:34.639054+0800 iOS多线程[4170:118478] queue 串行——Thread：<NSThread: 0x600000ef1cc0>{number = 1, name = main}
+     2019-12-10 12:11:34.639259+0800 iOS多线程[4170:118478] 元素：1----第0次
+     2019-12-10 12:11:34.639471+0800 iOS多线程[4170:118478] queue 串行——Thread：<NSThread: 0x600000ef1cc0>{number = 1, name = main}
+     2019-12-10 12:11:34.639667+0800 iOS多线程[4170:118478] 元素：2----第1次
+     2019-12-10 12:11:34.639881+0800 iOS多线程[4170:118478] queue 串行——Thread：<NSThread: 0x600000ef1cc0>{number = 1, name = main}
+     2019-12-10 12:11:34.640097+0800 iOS多线程[4170:118478] 元素：3----第2次
+     2019-12-10 12:11:34.640322+0800 iOS多线程[4170:118478] queue 串行——Thread：<NSThread: 0x600000ef1cc0>{number = 1, name = main}
+     2019-12-10 12:11:34.640526+0800 iOS多线程[4170:118478] 元素：4----第3次
+     */
+
+    
 }
 
 - (void)testSignl {//可实现类似 dispatch enter的功能。
